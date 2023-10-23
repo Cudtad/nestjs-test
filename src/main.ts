167dfb5ef1958dd08cc4b9f02eebdab3959fe116
DatND <datnd@DatND.local>
2023-10-23 17:06:57 +0700

    fix bug login

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true, //access-control-allow-credentials:true
+  };
+  app.use(cors(corsOptions));
   await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
