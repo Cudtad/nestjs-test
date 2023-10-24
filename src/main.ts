5c0ad12b2ad053de761a79867433baa4896b9a42
DatND <datnd@DatND.local>
2023-10-24 13:57:00 +0700

    fix origin

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://e-commerce-sand-xi.vercel.app'],
     credentials: true, //access-control-allow-credentials:true
   };
   app.use(cors(corsOptions));
