b6f1ddea1491dbc51856e5f6eaf74d9554ee533d
DatND <datnd@DatND.local>
2023-11-21 15:44:11 +0700

    update login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 
 import { UserService } from '../users/user.service';
 import { Payload } from '../types/payload';
 import { LoginDTO, RegisterDTO } from './auth.dto';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './auth.guard';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -14,27 +23,35 @@ export class AuthController {
   ) {}
 
   @Post('login')
-  async login(@Body() userDTO: LoginDTO) {
+  async login(
+    @Res({ passthrough: true }) res: Response,
+    @Body() userDTO: LoginDTO,
+  ) {
     const user = await this.userService.findByLogin(userDTO);
     const payload: Payload = {
       id: user._id,
       username: user.username,
       seller: user.seller,
     };
-    const token = await this.authService.signPayload(payload);
-    return { user, token };
+    const accessToken = await this.authService.signPayload(payload);
+    res.cookie('access-token', accessToken, {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+    });
+    return { user };
   }
 
   @Post('register')
   async register(@Body() userDTO: RegisterDTO) {
     const user = await this.userService.create(userDTO);
-    const payload: Payload = {
-      id: user._id,
-      username: user.username,
-      seller: user.seller,
-    };
-    const token = await this.authService.signPayload(payload);
-    return { user, token };
+    // const payload: Payload = {
+    //   id: user._id,
+    //   username: user.username,
+    //   seller: user.seller,
+    // };
+    // const token = await this.authService.signPayload(payload);
+    return { user };
   }
 
   @Get('me')
