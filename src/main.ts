b53d5f8d00b7685d920e0154621f3c419ae3c3ed
DatND <datnd@DatND.local>
2023-12-05 09:49:10 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions = {
-    origin: ['http://localhost:3000', 'https://e-commerce-sand-xi.vercel.app'],
+    origin: '*',
     credentials: true, //access-control-allow-credentials:true
   };
-  app.use(cors(corsOptions));
+
+  app.enableCors(corsOptions);
+
   await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
